7f7336ee1d4cfc6621e4ca1d5e427059814a0c99
Eunseong33 <jetaime0105@gmail.com>
2026-09-17 11:35:32 +0900

    [Passed] 일의 자리 배열 (141ms, 10MB)

---
+++ trail1/일의 자리 배열/array-with-units.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+       
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+      
+        System.out.print(a + " " + b + " ");
+
+        for (int i = 2; i < 10; i++){
+            int next = (a + b) % 10;
+            System.out.print(next + " ");
+
+            a = b;
+            b = next;
+        }
+    }
+}
