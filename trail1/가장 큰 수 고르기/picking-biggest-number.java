db944f2d38ade23e8f14746c2026835f81145f5b
Eunseong33 <jetaime0105@gmail.com>
2026-09-18 15:31:21 +0900

    [Passed] 가장 큰 수 고르기 (133ms, 10MB)

---
+++ trail1/가장 큰 수 고르기/picking-biggest-number.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < 10; i++){
+            int num = sc.nextInt();
+     
+            if (max < num) {
+                max = num;
+            }
+        }
+
+        System.out.print(max);
+    }
+}
