c97868f5ecd5bcd154a3bb03d9fc7497b726782b
Eunseong33 <jetaime0105@gmail.com>
2026-09-21 16:55:36 +0900

    [Passed] 두 개의 격자 비교하기 (131ms, 10MB)

---
+++ trail1/두 개의 격자 비교하기/compare-two-grid.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int[][] arr1 = new int[r][c];
+        int[][] arr2 = new int[r][c];
+        for (int i = 0; i < r; i++){
+            for (int j = 0; j < c; j++){
+                arr1[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < r; i++){
+            for (int j = 0; j < c; j++){
+                arr2[i][j] = sc.nextInt();
+                System.out.print((arr1[i][j] == arr2[i][j] ? '0' : '1') + " ");
+                }
+                System.out.println();
+            }
+            sc.close();
+        }
+    }
