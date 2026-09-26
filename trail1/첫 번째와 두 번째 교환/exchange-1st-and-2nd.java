8d00f728b8c3c98d3ce6ee440ce2e9e5c1aeeb22
Eunseong33 <jetaime0105@gmail.com>
2026-09-26 12:20:40 +0900

    [Passed] 첫 번째와 두 번째 교환 (119ms, 10MB)

---
+++ trail1/첫 번째와 두 번째 교환/exchange-1st-and-2nd.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        char[] arr = input.toCharArray();
+
+        char first = arr[0];
+        char second = arr[1];
+
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] == first){
+                arr[i] = second;
+            }
+
+            else if (arr[i] == second){
+                arr[i] = first;
+            }
+        }
+        System.out.print(arr);
+    }
+}
